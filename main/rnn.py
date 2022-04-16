3e4f349f86c287f4a1c9f1cd4bbeff3bc534a243
Zeng-Lecheng <50188852+Zeng-Lecheng@users.noreply.github.com>
2022-04-16 13:59:49 -0400

    [Modify](rnn.py) Random trying

--- main/rnn.py
+++ main/rnn.py
@@ -19,25 +19,38 @@ device = 'cpu'
 class RNNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.lstm = nn.LSTM(1, 16, 1, batch_first=True)
-        self.fc_1 = nn.Linear(16, 16)
-        self.fc_2 = nn.Linear(16, 12)
+        self.lstm_1 = nn.LSTM(1, 128, 1, batch_first=True)
+        self.lstm_2 = nn.LSTM(128, 32, 1, batch_first=True)
+        self.lstm_3 = nn.LSTM(32, 32, 1, batch_first=True)
+        self.rnn_1 = nn.RNN(1, 128, 1, batch_first=True)
+        self.drop_1 = nn.Dropout(0.5)
+        self.drop_2 = nn.Dropout(0.3)
+        self.fc_1 = nn.Linear(128, 32)
+        self.fc_2 = nn.Linear(32, 12)
         self.fc_3 = nn.Linear(12, 10)
 
     def forward(self, x):
-        lstm_out, hc = self.lstm(x)
-        h_0 = torch.relu(hc[0][0])
+        # ref: https://www.diva-portal.org/smash/get/diva2:1354738/FULLTEXT01.pdf
+        lstm_out, hc = self.rnn_1(x)
+        # lstm_out, hc = self.lstm_2(self.drop_1(lstm_out))
+        # lstm_out, hc = self.lstm_3(self.drop_2(lstm_out))
+        # h_0 = torch.relu(hc[0][0])
 
         ave_out = torch.sum(lstm_out, dim=1) / lstm_out.shape[1]
         # x = torch.relu(self.fc_1(h_0))
         x = torch.relu(self.fc_1(ave_out))
         x = torch.relu(self.fc_2(x))
-        return torch.sigmoid(self.fc_3(x))
+        x = torch.sigmoid(self.fc_3(x))
+        return x
 
 
-def model_train(learning_rate: float, batch_size: int, verbose: bool = False, test_while_train: bool = True) -> list[float]:
+def model_train(epochs: int,
+                learning_rate: float,
+                batch_size: int,
+                verbose: bool = False,
+                test_while_train: bool = True) -> list[float]:
     dataset = WavData('../data/genres_original', device=device)
-    train_size = int(len(dataset) * 0.8)
+    train_size = int(len(dataset) * 0.7)
     test_size = len(dataset) - train_size
     train_set, test_set = random_split(dataset, [train_size, test_size])
 
@@ -47,7 +60,7 @@ def model_train(learning_rate: float, batch_size: int, verbose: bool = False, te
     criterion = nn.CrossEntropyLoss().to(device)
 
     acc = []
-    for epoch in tqdm(range(1, 101)):
+    for epoch in tqdm(range(1, epochs + 1)):
         epoch_loss = 0
         for x_train, y_train in train_dataloader:
             optimizer.zero_grad()
@@ -58,15 +71,15 @@ def model_train(learning_rate: float, batch_size: int, verbose: bool = False, te
             epoch_loss += batch_loss.item() / len(x_train)
 
         if test_while_train:
-            epoch_acc = model_test(test_set, net)
+            epoch_acc = model_test(train_set, net)
             acc.append(epoch_acc)
-            # writer.add_scalar('Accuracy/test', epoch_acc, epoch)
+            writer.add_scalar('Accuracy/test', epoch_acc, epoch)
             if verbose:
                 print(f'Epoch: {epoch} Loss: {epoch_loss} Accuracy: {epoch_acc}')
         elif verbose:
             print(f'Epoch: {epoch} Loss: {epoch_loss}')
 
-        # writer.add_scalar('Loss/train', epoch_loss, epoch)
+        writer.add_scalar('Loss/train', epoch_loss, epoch)
 
     return acc
 
@@ -75,10 +88,12 @@ def model_test(test_set, net) -> float:
     with torch.no_grad():
         correct_count = 0
         count = 0
-        for x_test, y_test in DataLoader(test_set):
-            pred_test = net(x_test)
-            pred_label = (pred_test == pred_test.max())[0].nonzero()[0]
-            true_label = (y_test == 1)[0].nonzero()[0]
+        x_test, y_test = next(iter(DataLoader(test_set, batch_size=200, shuffle=True)))
+        pred_test = net(x_test)
+
+        for i in range(len(pred_test)):
+            pred_label = torch.argmax(pred_test[i])
+            true_label = torch.argmax(y_test[i])
             if pred_label.item() == true_label.item():
                 correct_count += 1
             count += 1
@@ -86,9 +101,6 @@ def model_test(test_set, net) -> float:
 
 
 if __name__ == '__main__':
-    acc = model_train(learning_rate=.0003,
-                      batch_size=50,
-                      verbose=False,
-                      test_while_train=True)
+    acc = model_train(epochs=5000, learning_rate=.0005, batch_size=50, verbose=False, test_while_train=True)
     plt.plot(range(1, len(acc) + 1), acc)
     plt.show()
