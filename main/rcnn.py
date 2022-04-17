cdcbf119fa007c40fa9288353b1811d918af55c3
EmilySu08 <esu@brandeis.edu>
2022-04-17 14:57:28 -0400

    still testing layers

--- main/rcnn.py
+++ main/rcnn.py
@@ -17,17 +17,17 @@ def train():
     train_data = int(len(PngData('../data/images_original'))* .8)
     test_data = len(PngData('../data/images_original')) - train_data
     train_set, test_set = random_split(PngData('../data/images_original'), [train_data, test_data])
-    dataloader = DataLoader(train_set, batch_size=10)
+    dataloader = DataLoader(train_set, batch_size=5)
 
     cel = nn.CrossEntropyLoss()
     total_loss = []
-    epoch_num = 100
+    epoch_num = 5#100
     rcnn_0 = RcnnNet().to('cpu')
     optimizer = optim.Adam(rcnn_0.parameters())
     for epoch in range(0, epoch_num + 1):  # loop over the dataset multiple times
         loss_in_epoch = []
         for i, (images, labels) in enumerate(dataloader, 0):
-            print(i)
+            #print(i)
             # get the inputs
             images = Variable(images.to('cpu'))
             labels = Variable(labels.to('cpu'))
@@ -42,17 +42,30 @@ def train():
         total_loss.append(mean(loss_in_epoch))
 
         accuracy = test(rcnn_0, test_set)
-        #print('For epoch', epoch + 1, 'the test accuracy over the whole test set is %f %%' % (accuracy))
+        print('For epoch', epoch + 1, 'the test accuracy over the whole test set is %f %%' % (accuracy))
     print(total_loss)
     print('Finished Training')
 #"""
 
 
 def test(net, test_set):
+    correct_count = 0
+    count = 0
+    x_test, y_test = next(iter(DataLoader(test_set, batch_size=5)))
+    pred_test = net(x_test)
+
+    for i in range(len(pred_test)):
+        pred_label = torch.argmax(pred_test[i])
+        true_label = torch.argmax(y_test[i])
+        if pred_label.item() == true_label.item():
+            correct_count += 1
+        count += 1
+    return correct_count / count
+    """
     net.eval()
-    count = 0.0
+    accuracy = 0.0
     total = 0.0
-    dataloader = DataLoader(test_set, batch_size=10)
+    dataloader = DataLoader(test_set, batch_size=5)
     with torch.no_grad():
         for i, (images, labels) in enumerate(dataloader, 0):
             print(i)
@@ -64,12 +77,11 @@ def test(net, test_set):
             # highest label represents the predictions
             _, prediction = torch.max(outputs.data, 1)
             total += labels.size(0)
-            for j in range(0, len(labels)):
-                if labels[j] == prediction[j]:
-                    count += 1
+            accuracy += (prediction == labels).sum().item()
                     # compute the accuracy over all test images
-    accuracy = (100 * count / total)
+    accuracy = (100 * accuracy / total)
     return accuracy
+"""
 #"""
 
 
@@ -77,23 +89,29 @@ class RcnnNet(nn.Module):  # have to change numbers depending on data
     def __init__(self):
         super(RcnnNet, self).__init__()
 
-        self.conv1 = nn.Conv2d(3, 8, 3, 1)
+        self.conv1 = nn.Conv2d(4, 10, 288, 432, groups=2)
+        #self.conv2 = nn.Conv2d(10, 64, 288, 432, groups=2)
+        """
+        self.conv1 = nn.Conv2d(3, 8, 3)
         self.pool = nn.AdaptiveMaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(8, 16, 3, 1)
+        self.conv2 = nn.Conv2d(8, 16, 3)
 
-        self.conv3 = nn.Conv2d(16, 32, 3, 1)
-        self.conv4 = nn.Conv2d(32, 64, 3, 1)
+        self.conv3 = nn.Conv2d(16, 32, 3)
+        self.conv4 = nn.Conv2d(32, 64, 3)
         self.pool = nn.AdaptiveMaxPool2d(2, 2)
 
         self.fc_1 = nn.Linear(64 * 56 * 56, 120)
         self.fc_2 = nn.Linear(120, 64)
         self.fc_3 = nn.Linear(64, 2)
         self.Sigmoid = nn.Sigmoid()
+        """
+
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-
-        x_pre_input = x
+        x = self.conv1(x)
+        #x = self.conv2(x)
+        """
+         x_pre_input = x
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
         # residual connection
@@ -105,6 +123,8 @@ class RcnnNet(nn.Module):  # have to change numbers depending on data
         x = F.relu(self.fc_1(x))
         x = F.relu(self.fc_2(x))
         x = F.sigmoid(self.fc_3(x))
+        """
+
 
         return x
 
