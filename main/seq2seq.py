0ae23b7afd75bd6bcd84f6d30f31cba318d6ec19
ejdiener <64671864+ejdiener@users.noreply.github.com>
2022-04-19 10:01:13 -0400

    I forget what changes I made to dnn but for seq2seq I made the embedding txt file.

--- main/seq2seq.py
+++ main/seq2seq.py
@@ -1,30 +1,35 @@
+import os
 import numpy as np
-import matplotlib.pyplot as plt
-# import scipy
+from pathlib import Path
+from towhee import pipeline
+import csv
+from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 import util
 
 
 def main():
     data = DataLoader(util.WavData('../data/genres_original'))
-    x = next(iter(data))
-    print(x.shape)
 
 
 def model():
     pass
 
+
 def test():
     pass
+
 
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
         # Initialize the layers
+
 
 
     def forward(self, x):
@@ -66,4 +71,25 @@ class AttentionDecoder(Decoder):
 
         return x
 
-main()
+def getVGGishEmbeddings(filepath = '../data/vggish'):
+    embedding_pipeline = pipeline('towhee/audio-embedding-vggish')
+    dataset_path = filepath
+    music_list = [f for f in Path(dataset_path).glob('*')]
+    vec_sets = []
+    with tqdm(total=999) as progressbar:
+        for audio_path in music_list:
+            vecs = embedding_pipeline(str(audio_path))
+            norm_vecs = [vec / np.linalg.norm(vec) for vec in vecs[0][0]]
+            vec_sets.append(norm_vecs)
+            progressbar.update(1)
+    print(len(vec_sets))
+    print(vec_sets)
+    vectors = []
+    for i in tqdm(range(len(vec_sets))):
+        for vec in vec_sets[i]:
+            vectors.append(vec)
+    print(vectors)
+    with open('embeddings.txt', 'w') as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerows(vectors)
+
