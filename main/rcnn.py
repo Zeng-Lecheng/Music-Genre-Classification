b8cfe45f5186b4127ff111dd8518ef9ef912321e
EmilySu08 <esu@brandeis.edu>
2022-04-20 17:47:11 -0400

    fixed model

--- main/rcnn.py
+++ main/rcnn.py
@@ -1,11 +1,12 @@
 import pickle
 
 import numpy as np
-import matplotlib.pyplot as plt
+
 # import scipy
 #import rcnn_utils as U
 import torch
 from torch.utils.data import DataLoader, random_split
+import matplotlib.pyplot as plt
 from main.util import PngData
 import torch.nn as nn
 from torch.autograd import Variable
@@ -17,25 +18,25 @@ torch.manual_seed(0)
 
 def train():
     png_data = pickle.load(open('png_data.pkl', 'rb'))
-    train_data = int(len(png_data)* .7)#/3
+    train_data = int(len(png_data)* .8)#/3
 
     test_data = len(png_data) - train_data
 
     train_set, test_set = random_split(png_data, [int(train_data), int(test_data)])
 
-    dataloader = DataLoader(train_set, batch_size=10)
+    dataloader = DataLoader(train_set, batch_size=30)
     #print(train_set.shape())
     #"""
 
     cel = nn.BCELoss()
     total_loss = []
-    epoch_num = 10#100
+    epoch_num = 30
     rcnn_0 = RcnnNet().to('cpu')
-    optimizer = optim.Adam(rcnn_0.parameters(), lr=0.0001)
+    #optimizer = optim.SGD(rcnn_0.parameters(), lr=0.0001)
+    optimizer = optim.Adam(rcnn_0.parameters(), lr=0.01)
     for epoch in range(0, epoch_num + 1):  # loop over the dataset multiple times
         loss_in_epoch = []
         for i, (images, labels) in enumerate(dataloader, 0):
-            #print(i)
             # get the inputs
             images = Variable(images.to('cpu'))
             labels = Variable(labels.to('cpu'))
@@ -49,17 +50,19 @@ def train():
             loss_in_epoch.append(loss.item())
         total_loss.append(mean(loss_in_epoch))
 
-        #accuracy = test(rcnn_0, test_set)
+        accuracy = test(rcnn_0, test_set)
         print('For epoch', epoch + 1, 'the test accuracy over the whole test set is %f %%' % (accuracy))
     print(total_loss)
     print('Finished Training')
+    return total_loss
 
 
 
 def test(net, test_set):
+
     correct_count = 0
     count = 0
-    x_test, y_test = next(iter(DataLoader(test_set, batch_size=10)))
+    x_test, y_test = next(iter(DataLoader(test_set, batch_size=300)))
     pred_test = net(x_test)
 
     for i in range(len(pred_test)):
@@ -71,67 +74,50 @@ def test(net, test_set):
     return correct_count / count
 
 
-"""
-
-
-    net.eval()
-    accuracy = 0.0
-    total = 0.0
-    dataloader = DataLoader(test_set, batch_size=5)
-    with torch.no_grad():
-        for i, (images, labels) in enumerate(dataloader, 0):
-            print(i)
-            # get the inputs
-            images = Variable(images.to('cpu'))
-            labels = Variable(labels.to('cpu'))
-            # run the model on the test set to predict labels
-            outputs = net(images)  #
-            # highest label represents the predictions
-            _, prediction = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            accuracy += (prediction == labels).sum().item()
-                    # compute the accuracy over all test images
-    accuracy = (100 * accuracy / total)
-    return accuracy
-
-"""
-
-
 class RcnnNet(nn.Module):  # have to change numbers depending on data
     def __init__(self):
         super(RcnnNet, self).__init__()
 
         self.conv1 = nn.Conv2d(4, 10, 1)
-        self.pool = nn.AdaptiveMaxPool2d(2, 2)
+        self.pool = nn.AdaptiveMaxPool2d(2)
         self.conv2 = nn.Conv2d(10, 10, 1)
         self.conv3 = nn.Conv2d(10, 10, 1)
         self.conv4 = nn.Conv2d(10, 64, 1)
-        self.pool = nn.AdaptiveMaxPool2d(2, 2)
+        #self.pool = nn.AdaptiveMaxPool2d(2, 2)
 
-        self.fc_1 = nn.Linear(64 * 216 * 576, 120)
+        self.fc_1 = nn.Linear(256, 120)
         self.fc_2 = nn.Linear(120, 64)
-        self.fc_3 = nn.Linear(64, 2)
+        self.fc_3 = nn.Linear(64, 10)
         self.Sigmoid = nn.Sigmoid()
 
-
     def forward(self, x):
-        x = F.relu(self.conv1(x))
+        x = F.relu(self.pool(self.conv1(x)))
         x_pre_input = x
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
         # residual connection
         x = x + x_pre_input
-        x = F.relu(self.conv4(x))
+        x = F.relu(self.pool(self.conv4(x)))
 
         # flattens tensor
         x = x.view(x.size(0), -1)  # number of samples in batch
         x = F.relu(self.fc_1(x))
         x = F.relu(self.fc_2(x))
-        x = F.sigmoid(self.fc_3(x))
+        x = torch.sigmoid(self.fc_3(x))
 
         return x
 
 
-train()
+if __name__ == '__main__':
+    result_1 = train()
+    plt.plot(result_1, 'g', label='Adam Optimizer (learning rate = .001)')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+
+
+
 
 
