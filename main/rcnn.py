3509be84177793bc56c14abab94a8bc457990076
EmilySu08 <esu@brandeis.edu>
2022-04-22 18:04:33 -0400

    added dropout layer

--- main/rcnn.py
+++ main/rcnn.py
@@ -15,16 +15,18 @@ import torch.nn.functional as F
 from statistics import mean
 torch.manual_seed(0)
 
-
-def train(optimizer, net):
+def data_loader():
     png_data = pickle.load(open('png_data.pkl', 'rb'))
-    train_data = int(len(png_data)* .8)#/3
+    train_data = int(len(png_data) * .8)  # /3
 
     test_data = len(png_data) - train_data
 
     train_set, test_set = random_split(png_data, [int(train_data), int(test_data)])
+    return train_set, test_set
 
-    dataloader = DataLoader(train_set, batch_size=20)
+def train(optimizer, net, train_set, test_set):
+    net.train()
+    dataloader = DataLoader(train_set, batch_size=20, shuffle = True)
     #print(train_set.shape())
     #"""
 
@@ -58,12 +60,31 @@ def train(optimizer, net):
 
 
 def test(net, test_set):
-
-    correct_count = 0
-    count = 0
+    net.eval()
+    batch_size = 20
     x_test, y_test = next(iter(DataLoader(test_set, batch_size=20)))
-    pred_test = net(x_test)
+    #pred_test = net(x_test)
+    count = 0.0
+    total = 0.0
 
+    with torch.no_grad():
+        for j in range(0, batch_size):
+            inputs = x_test
+            labels = y_test
+            # run the model on the test set to predict labels
+            outputs = net(inputs)  #
+            # highest label represents the predictions
+            _, prediction = torch.max(outputs.data, 1)
+            _, actual = torch.max(labels.data, 1)
+            total += labels.size(0)
+            for i in range(0, len(labels)):
+                if actual[i] == prediction[i]:
+                    count += 1
+                    # compute the accuracy over all test images
+    accuracy = (100 * count / total)
+    return accuracy
+
+    """
     for i in range(len(pred_test)):
         pred_label = torch.argmax(pred_test[i])
         true_label = torch.argmax(y_test[i])
@@ -71,7 +92,7 @@ def test(net, test_set):
             correct_count += 1
         count += 1
     return correct_count / count
-
+"""
 
 class RcnnNet(nn.Module):  # have to change numbers depending on data
     def __init__(self):
@@ -86,6 +107,7 @@ class RcnnNet(nn.Module):  # have to change numbers depending on data
 
         self.fc_1 = nn.Linear(256, 120)
         self.fc_2 = nn.Linear(120, 64)
+        self.dropout = nn.dropout(.5)
         self.fc_3 = nn.Linear(64, 10)
         self.Sigmoid = nn.Sigmoid()
 
@@ -101,6 +123,7 @@ class RcnnNet(nn.Module):  # have to change numbers depending on data
         # flattens tensor
         x = x.view(x.size(0), -1)  # number of samples in batch
         x = F.relu(self.fc_1(x))
+        x = self.dropout(x)
         x = F.relu(self.fc_2(x))
         x = torch.sigmoid(self.fc_3(x))
 
@@ -109,11 +132,10 @@ class RcnnNet(nn.Module):  # have to change numbers depending on data
 
 if __name__ == '__main__':
     rcnn_0 = RcnnNet()#.to('Gpu')
-    #optimizer =
-    # optimizer = optim.Adam(rcnn_0.parameters(), lr=0.01)
-    result_1 = train(optim.SGD(rcnn_0.parameters(), lr=0.001, weight_decay=1), rcnn_0)
-    result_2 = train(optim.Adam(rcnn_0.parameters(), lr=0.001, weight_decay=1), rcnn_0)
-    result_3 = train(optim.Adagrad(rcnn_0.parameters(), lr=0.001, weight_decay=1), rcnn_0)
+    train_set, test_set = data_loader()
+    result_1 = train(optim.SGD(rcnn_0.parameters(), lr=0.001, weight_decay=.01), rcnn_0, train_set, test_set)
+    result_2 = train(optim.Adam(rcnn_0.parameters(), lr=0.001, weight_decay=.01), rcnn_0, train_set, test_set)
+    result_3 = train(optim.Adagrad(rcnn_0.parameters(), lr=0.001, weight_decay=.01), rcnn_0, train_set, test_set)
     plt.plot(result_1, 'g', label='SGD')
     plt.plot(result_2, 'b', label='Adam')
     plt.plot(result_3, 'r', label='ADA')
