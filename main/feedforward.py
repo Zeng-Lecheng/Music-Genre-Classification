85618f220b21f1d466b46e0136ca4ced73c38066
ejdiener <64671864+ejdiener@users.noreply.github.com>
2022-04-27 08:59:03 -0400

    loading in all neural networks

--- main/feedforward.py
+++ main/feedforward.py
@@ -1,17 +1,35 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import pickle as pkl
 
+import util
 from rnn import RNNet
 from rcnn import RcnnNet
 from dnn import DeepNeuralNet
+from util import WavData
 
 # from torch.utils.tensorboard import SummaryWriter
 # writer = SummaryWriter()
 
 device = 'cpu'  # in case nvidia GPU is available, change to 'cuda' might accelerate
+
+
+
+def get_data():
+    feat_data, feat_labels, length = util.loadCSV(f'../data/features_30_sec.csv')
+    png_data = pkl.load(open('png_data.pkl', 'rb'))     #PngData
+    wav_data = WavData('../data/genres_original', device=device)
+    loader = DataLoader(png_data, length)
+    png_inputs, png_labels = next(iter(loader))
+    loader = DataLoader(wav_data, length)
+    wav_inputs, wav_labels = next(iter(loader))
+
+    for i in tqdm(range(0, 999)):
+        pass
 
 
 def model_train(train: bool, verbose: bool):
